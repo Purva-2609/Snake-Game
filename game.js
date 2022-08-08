15c5f4c17a953fba0f9783ea246bd14cce5fd570
Purva-2609 <110804003+Purva-2609@users.noreply.github.com>
2022-08-08 10:35:45 +0530

    Initial commit

---
+++ game.js
@@ -0,0 +1,45 @@
+let direction = {x: 0, y: 0};
+/*const foodSound = new Audio('Food.mp3');
+const gameOverSound = new Audio('gameover.mp3');
+const moveSound = new Audio('move.mp3');
+const musicSound = new Audio('music.mp3');*/
+let speed = 2;
+let lastPaintTime = 0;
+let snakeArr = [
+    {x: 13, y: 15}
+]
+food = {x: 6 , y: 7};
+// Functions
+function main(ctime) {
+    window.requestAnimationFrame(main);
+    console.log(ctime)
+    if((ctime - lastPaintTime)/1000 < 1/speed){
+        return;
+    }
+    lastPaintTime = ctime;
+    gameEngine();
+}
+function gameEngine(){  
+    //display te snake
+    board.innerHTML = "";
+    snakeArr.forEach((e, index)=>{
+        snakeElement = document.createElement('div');
+        snakeElement.style.gridRowStart = e.y;
+        snakeElement.style.gridColumnStart = e.x;
+        snakeElement.classList.add('head')
+        board.appendChild(snakeElement);
+    });
+  
+}
+  
+
+
+
+
+
+
+
+
+
+// Logic
+window.requestAnimationFrame(main);
